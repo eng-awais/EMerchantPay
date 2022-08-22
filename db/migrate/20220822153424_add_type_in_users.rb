e0910041e25848c3fe1cff60c587b5e03722a932
awais <devawais.cs@gmail.com>
2022-08-23 01:11:42 +0500

    updated migrations

--- db/migrate/20220822153424_add_type_in_users.rb
+++ db/migrate/20220822153424_add_type_in_users.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
 class AddTypeInUsers < ActiveRecord::Migration[7.0]
-  def up
+  def change
     add_column :users, :type, :integer, null: false
     remove_column :users, :role, :integer, null: false
-  end
-
-  def down
-    remove_column :users, :type, :integer, null: false
-    add_column :users, :role, :integer, null: false
   end
 end
--- db/migrate/20220822163518_remove_user_foreignkey_and_add_merchant_user_foreign_key.rb
+++ db/migrate/20220822163518_remove_user_foreignkey_and_add_merchant_user_foreign_key.rb
@@ -2,7 +2,7 @@
 
 class RemoveUserForeignkeyAndAddMerchantUserForeignKey < ActiveRecord::Migration[7.0]
   def change
-    remove_index :merchants, column: [:user_id]
+    remove_index :merchants, :user_id
     remove_column :merchants, :user_id, :uuid
     add_reference :merchants, :merchant_user, type: :uuid, index: true
   end
